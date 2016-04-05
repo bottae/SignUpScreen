5428747a3add5fb7542e40ee30ea79f4002a1c18
bottae <bottae@gmail.com>
2016-04-06 08:06:48 +0900

    update

--- SignUpScreen/ViewController.swift
+++ SignUpScreen/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var idTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var telTextField: UITextField!
+    @IBOutlet var blogTextField: UITextField!
+    @IBOutlet var resultTextView: UITextView!
+    
+    
+    @IBAction func signUpAction(sender: AnyObject) {
+        resultTextView.text = "\(nameTextField.text!) 님 가입을 축하드립니다"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
