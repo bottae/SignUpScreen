96adf6a5e515bcc4be3ce6574db6c4cae57fdb2e
bottae <bottae@gmail.com>
2016-04-06 10:54:47 +0900

    final

--- SignUpScreen/ViewController.swift
+++ SignUpScreen/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     
     
@@ -19,14 +19,31 @@ class ViewController: UIViewController {
     @IBOutlet var blogTextField: UITextField!
     @IBOutlet var resultTextView: UITextView!
     
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("textFieldShouldReturn")
+        textField.resignFirstResponder()
+        return true
+    }
+    
+
     
     @IBAction func signUpAction(sender: AnyObject) {
         resultTextView.text = "\(nameTextField.text!) 님 가입을 축하드립니다"
     }
     
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // important!!!
+        nameTextField.delegate = self
+        idTextField.delegate = self
+
+        
     }
 
     override func didReceiveMemoryWarning() {
